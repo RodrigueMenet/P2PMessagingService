c48a267e826d52decc5a6a0b1c32fb30cac3c087
rodrigue.menet <rodrigue.menet@hexagon.com>
2022-05-13 16:34:28 +0200

    UTs skeleton

---
+++ P2PNativeClientA.Tests/P2PNativeClientA.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace P2PNativeClientATests
+{
+	TEST_CLASS(P2PNativeClientATests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ P2PServer.Tests/P2PServer.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace P2PServerTests
+{
+	TEST_CLASS(P2PServerTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
